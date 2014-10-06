6ddb5dd80ec3e9f2299d3789fd634d9d5bcaa574
Carlos <Carlos@Xapa_Desktop>
2014-10-06 14:41:19 +0200

    CR 1.0.5

--- src/main/java/HolaMundo.java
+++ src/main/java/HolaMundo.java
@@ -5,6 +5,7 @@ public class HolaMundo {
     //CR 1.0.2
     //CR 1.0.3
     //CR 1.0.4
+    //CR 1.0.5
     public static void main(String[] args){
         System.out.println("Hola Mundo!");
     }
